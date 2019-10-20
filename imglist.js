8e7871fc22b3092eea1d0e22b2b9e2f2650756d7
harrykwan <33515754+harrykwan@users.noreply.github.com>
2019-10-21 03:56:52 +0800

    Add files via upload

---
+++ imglist.js
@@ -0,0 +1,32 @@
+var imglist = {
+	"Introduction" : {description: "‘Seek What sets your soul on fire’<br>			<br>			A London based Hong Kong Fashion Textile designer. Studying in University the Arts of London -London college of Fashion (LCF) <br>			Fashion textile: Embroidery. <br>			Specializing in Beading and Blind stitch<br>			Having keen eye on attention to details, a touch of feminine vibe and sense of aesthetic. <br>			<br>			‘People will stare. Make it worth their While’ -Harry Winston<br>"},
+	"Inspiration" : {
+		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/5b/5b/41/5b5b41caefe28908f5e124c26b0f5431.jpg","https://i.pinimg.com/originals/c7/e5/07/c7e5075308961f39854a631241eb4c7e.jpg"], 
+		description: "asdf"
+	},
+	"Natural Dye" : {
+		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
+		description: "asdf"
+	},
+	"Beading" : {
+		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
+		description: "asdf"
+	},
+	"Blind Stitch" : {
+		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
+		description: "asdf"
+	},
+	"CAD" : {
+		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
+		description: "asdf"
+	},
+	"Weaving" : {
+		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
+		description: "asdf"
+	},
+	"Collaboration" : {
+		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
+		description: "asdf"
+	},
+	"Contact" : {}
+}
