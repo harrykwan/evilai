6a0a58de98e89bcd77b9fcfd4906fcff838e61ff
crystalevilai <56803060+crystalevilai@users.noreply.github.com>
2019-10-20 23:04:38 +0100

    Update imglist.js

--- imglist.js
+++ imglist.js
@@ -31,8 +31,11 @@ var imglist = {
 		description: "asdf"
 	},
 	"Weaving" : {
-		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg",
-		      "https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
+		url: ["https://i.pinimg.com/originals/04/77/41/04774175b627cadbd8f83df80bfb3bcd.jpg",
+		      "https://i.pinimg.com/originals/fc/77/02/fc77023dd24a253fd1e81fd7ef11bf6d.jpg",
+		     "https://i.pinimg.com/originals/01/96/b4/0196b4c18c10c0e28d68ae7e8a038686.jpg",
+		     "https://i.pinimg.com/originals/b3/32/33/b3323375de318df8ae7294d44b92cab3.jpg",
+                     "https://i.pinimg.com/originals/cc/33/dd/cc33dde79d3291a3f83747f93352afa7.jpg"], 
 		description: "<br>Dye 100%cotton with natural plants, <br>Coffee,Spinach,Purple Dragon Fruit,Isatidis Radix, <br> ripped into stripes,weave them into garment,is a way to convey the idea of sustainability, while it cause no damage for the planet after dissolving into soil"
 	},
 	"Collaboration" : {
