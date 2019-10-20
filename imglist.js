279a2541bd7273a627220d7fec1bb7836e79685d
crystalevilai <56803060+crystalevilai@users.noreply.github.com>
2019-10-21 04:22:11 +0800

    Update imglist.js

--- imglist.js
+++ imglist.js
@@ -1,5 +1,7 @@
 var imglist = {
-	"Introduction" : {description: "‘Seek What sets your soul on fire’<br>			<br>			A London based Hong Kong Fashion Textile designer. Studying in University the Arts of London -London college of Fashion (LCF) <br>			Fashion textile: Embroidery. <br>			Specializing in Beading and Blind stitch<br>			Having keen eye on attention to details, a touch of feminine vibe and sense of aesthetic. <br>			<br>			‘People will stare. Make it worth their While’ -Harry Winston<br>"},
+	"Introduction" : {
+		description: "‘Seek What sets your soul on fire’<br>			<br>			A London based Hong Kong Fashion Textile designer. Studying in University the Arts of London -London college of Fashion (LCF) <br>			Fashion textile: Embroidery. <br>			Specializing in Beading and Blind stitch<br>			Having keen eye on attention to details, a touch of feminine vibe and sense of aesthetic. <br>			<br>			‘People will stare. Make it worth their While’ -Harry Winston<br>"
+	},
 	"Inspiration" : {
 		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/5b/5b/41/5b5b41caefe28908f5e124c26b0f5431.jpg","https://i.pinimg.com/originals/c7/e5/07/c7e5075308961f39854a631241eb4c7e.jpg"], 
 		description: "asdf"
@@ -13,8 +15,10 @@ var imglist = {
 		description: "asdf"
 	},
 	"Blind Stitch" : {
-		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
-		description: "asdf"
+		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg",
+		      "https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg",
+		     "https://i.pinimg.com/originals/48/13/e1/4813e1629998d432bf75497e1ec221e6.jpg"], 
+		description: "asdf<br>fdsa"
 	},
 	"CAD" : {
 		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg","https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
