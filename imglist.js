856b8fab3b8cbb181ae8fb80c7af71a2471cb25f
harrykwan <33515754+harrykwan@users.noreply.github.com>
2019-10-24 03:02:29 +0800

    Update imglist.js

--- imglist.js
+++ imglist.js
@@ -1,16 +1,15 @@
 var imglist = {
 	"Introduction" : {
-		description: "‘Seek What sets your soul on fire’<br>			<br>			A London based Hong Kong Fashion Textile designer. Studying in University the Arts of London -London college of Fashion (LCF) <br>			Fashion textile: Embroidery. <br>			Specializing in Beading and Blind stitch<br>			Having keen eye on attention to details, a touch of feminine vibe and sense of aesthetic. <br>			<br>			‘People will stare. Make it worth their While’ -Harry Winston<br>"
-	},
+		description: ["‘Seek What sets your soul on fire’<br>			<br>			A London based Hong Kong Fashion Textile designer. Studying in University the Arts of London -London college of Fashion (LCF) <br>			Fashion textile: Embroidery. <br>			Specializing in Beading and Blind stitch<br>			Having keen eye on attention to details, a touch of feminine vibe and sense of aesthetic. <br>			<br>			‘People will stare. Make it worth their While’ -Harry Winston<br>"]	},
 	"Inspiration" : {
-		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg",
+		url: [["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg",
 		      "https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg",
 		      "https://i.pinimg.com/originals/5b/5b/41/5b5b41caefe28908f5e124c26b0f5431.jpg",
-		      "https://i.pinimg.com/originals/c7/e5/07/c7e5075308961f39854a631241eb4c7e.jpg"], 
-		description: "asdf"
+		      "https://i.pinimg.com/originals/c7/e5/07/c7e5075308961f39854a631241eb4c7e.jpg"]], 
+		description: ["asdf"]
 	},
 	"Natural Dye" : {
-		url: ["https://i.pinimg.com/originals/49/79/75/497975a457148d3e38fd96bb529fcd57.jpg",
+		url: [["https://i.pinimg.com/originals/49/79/75/497975a457148d3e38fd96bb529fcd57.jpg",
 		      "https://i.pinimg.com/originals/74/7c/4c/747c4cc4a5e74a00720b84293d8214ae.jpg",
 		      "https://i.pinimg.com/originals/81/0d/56/810d56e0ffb4cddb2113874daf2b64ae.jpg",
 		      "https://i.pinimg.com/originals/33/dd/94/33dd94eb68ab93b7805d6b51a703420a.jpg",
@@ -19,50 +18,52 @@ var imglist = {
 		      "https://i.pinimg.com/originals/42/14/81/421481360c13d9dd572c7fc281cddb1b.jpg",
 		      "https://i.pinimg.com/originals/24/1b/fc/241bfc8d46d4813afa9db7678c7a16a2.jpg",
 		      "https://i.pinimg.com/originals/09/74/08/0974088c16a163b114784067ccbfcb38.jpg",
-		      "https://i.pinimg.com/originals/3d/ee/60/3dee60e026dc5a585239860761cd195e.jpg",], 
-		description: "asdf"
+		      "https://i.pinimg.com/originals/3d/ee/60/3dee60e026dc5a585239860761cd195e.jpg"]], 
+		description: ["asdf"]
 	},
 	"Beading" : {
-		url: ["https://i.pinimg.com/originals/c2/74/ac/c274ac63b7126e477363f8dd43cd80f7.jpg",
+		url: [["https://i.pinimg.com/originals/c2/74/ac/c274ac63b7126e477363f8dd43cd80f7.jpg",
 		      "https://i.pinimg.com/originals/15/f0/77/15f077dfc3b6a4151e495d1ee02e843d.jpg",
 		      "https://i.pinimg.com/originals/62/c9/6b/62c96bb6eaf56787d235389731559936.jpg",
 		      "https://i.pinimg.com/originals/6a/c3/46/6ac3469fde2feee288c379b3df628cb8.jpg",
 		      "https://i.pinimg.com/originals/12/73/8b/12738be7eb145d9b98b5fea553ad18fa.jpg",
 		      "https://i.pinimg.com/originals/27/8b/90/278b90391b356b4b1efd132944e6049f.jpg",
 		      "https://i.pinimg.com/originals/2e/a3/b6/2ea3b6cde47f10772d57972eabafa87f.jpg",
-		      "https://i.pinimg.com/originals/56/cc/37/56cc37e25d5264e3faa7fd89d4d9a8ac.jpg"], 
-		description: "asdf"
+		      "https://i.pinimg.com/originals/56/cc/37/56cc37e25d5264e3faa7fd89d4d9a8ac.jpg"]], 
+		description: ["asdf"]
 	},
 	"Blind Stitch" : {
-		url: ["https://i.pinimg.com/originals/93/a6/16/93a6163ab4d04022549c6ba5147c87e3.jpg",
+		url: [["https://i.pinimg.com/originals/93/a6/16/93a6163ab4d04022549c6ba5147c87e3.jpg",
 		      "https://i.pinimg.com/originals/fa/08/ec/fa08ec1e0d056452b1bdbe1841137ed5.jpg",
 		      "https://i.pinimg.com/originals/0f/a4/c0/0fa4c02bda65d6388950196b161801d9.jpg",
-		      "https://i.pinimg.com/originals/47/19/76/4719763849a8776e96e0288e3fbdf380.jpg",
-	              "https://i.pinimg.com/originals/11/17/b8/1117b8aec2eadc52ddee2085bd0d11f6.jpg",
+			  "https://i.pinimg.com/originals/47/19/76/4719763849a8776e96e0288e3fbdf380.jpg"],
+
+			  ["https://i.pinimg.com/originals/11/17/b8/1117b8aec2eadc52ddee2085bd0d11f6.jpg",
 		      "https://i.pinimg.com/originals/fc/58/de/fc58def8c48599a95e5d54123dc42340.jpg",
 		      "https://i.pinimg.com/originals/65/73/f9/6573f934ef59428385b04b39a9ef2c11.jpg",
 		      "https://i.pinimg.com/originals/a1/ca/d6/a1cad654b9047002cd78a5e369368013.jpg",
 		      "https://i.pinimg.com/originals/11/bf/5d/11bf5dbceff4c426d2262e4a9212315a.jpg",
 		      "https://i.pinimg.com/originals/b9/42/2d/b9422d2e604401c7403b7428b2364fb6.jpg",
 		      "https://i.pinimg.com/originals/3b/7c/8d/3b7c8d3be23b594b43a1e226d356dcd9.jpg",
-		      "https://i.pinimg.com/originals/35/dc/6b/35dc6b442d576f488d275e3bd20631e6.jpg"], 
-		description: " Collaboration project<br>Bespoke tailoring : Michael WANG<br>Fashion Textile : Evian LAI<br>Model :Denny SHU <br>“I may not be free, but I’m not about to surrender the illusion of choice” -Jonny Rich<br>A symbolisation of the predetermined lifestyle under Asian parenting ,imitating the restrictive predetermined life of a cocoon, yet the next generations are going to inherit this lifestyle<br>Technique used:<br>Blind stitch <br>Canadian Smocking "
+		      "https://i.pinimg.com/originals/35/dc/6b/35dc6b442d576f488d275e3bd20631e6.jpg"]], 
+		description: [" Collaboration project<br>Bespoke tailoring : Michael WANG<br>Fashion Textile : Evian LAI<br>Model :Denny SHU <br>“I may not be free, but I’m not about to surrender the illusion of choice” -Jonny Rich<br>A symbolisation of the predetermined lifestyle under Asian parenting ,imitating the restrictive predetermined life of a cocoon, yet the next generations are going to inherit this lifestyle<br>Technique used:<br>Blind stitch <br>Canadian Smocking ",
+					"asdfasdf sec description"]
 	},
 	"CAD" : {
-		url: ["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg",
-		      "https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"], 
-		description: "asdf"
+		url: [["https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg",
+		      "https://i.pinimg.com/originals/a5/03/f9/a503f982e78c2e3fb19ff45f9b2125cd.jpg"]], 
+		description: ["asdf"]
 	},
 	"Weaving" : {
-		url: ["https://i.pinimg.com/originals/04/77/41/04774175b627cadbd8f83df80bfb3bcd.jpg",
+		url: [["https://i.pinimg.com/originals/04/77/41/04774175b627cadbd8f83df80bfb3bcd.jpg",
 		      "https://i.pinimg.com/originals/fc/77/02/fc77023dd24a253fd1e81fd7ef11bf6d.jpg",
 		     "https://i.pinimg.com/originals/01/96/b4/0196b4c18c10c0e28d68ae7e8a038686.jpg",
 		     "https://i.pinimg.com/originals/b3/32/33/b3323375de318df8ae7294d44b92cab3.jpg",
-                     "https://i.pinimg.com/originals/cc/33/dd/cc33dde79d3291a3f83747f93352afa7.jpg"], 
-		description: "<br>Dye 100%cotton with natural plants, <br>Coffee,Spinach,Purple Dragon Fruit,Isatidis Radix, <br> ripped into stripes,weave them into garment,is a way to convey the idea of sustainability, while it cause no damage for the planet after dissolving into soil"
+                     "https://i.pinimg.com/originals/cc/33/dd/cc33dde79d3291a3f83747f93352afa7.jpg"]], 
+		description: ["<br>Dye 100%cotton with natural plants, <br>Coffee,Spinach,Purple Dragon Fruit,Isatidis Radix, <br> ripped into stripes,weave them into garment,is a way to convey the idea of sustainability, while it cause no damage for the planet after dissolving into soil"]
 	},
 	"Collaboration" : {
-		url: ["https://i.pinimg.com/originals/2f/4e/27/2f4e279a12d2a189ae4d739e3d398f09.jpg",
+		url: [["https://i.pinimg.com/originals/2f/4e/27/2f4e279a12d2a189ae4d739e3d398f09.jpg",
 		      "https://i.pinimg.com/originals/ed/b6/7b/edb67b7768cf4ea2850d71d09890bda1.jpg",
 		      "https://i.pinimg.com/originals/56/3a/98/563a98ee66beb209be6faad3181713ca.jpg",
 		      "https://i.pinimg.com/originals/bf/ed/d7/bfedd78357313858a6e343bab3618fba.jpg",
@@ -79,10 +80,10 @@ var imglist = {
 		      "https://i.pinimg.com/originals/09/ff/97/09ff97ef676fa6184b351db4a2f61376.jpg",
 		      "https://i.pinimg.com/originals/9e/01/cd/9e01cd61a77abf4187d9dc2217b4deca.jpg",
 		      "https://i.pinimg.com/originals/f1/85/3e/f1853e34d8d64b72c4e090c2da34fcd9.jpg",
-		      "https://i.pinimg.com/originals/18/5b/9e/185b9e455163d232a638810fbf49ef07.jpg"], 
-		description: "asdf"
+		      "https://i.pinimg.com/originals/18/5b/9e/185b9e455163d232a638810fbf49ef07.jpg"]], 
+		description: ["asdf"]
 	},
 	"Contact" : {
-		description: "asdf"
+		description: ["asdf"]
 	}
 }
